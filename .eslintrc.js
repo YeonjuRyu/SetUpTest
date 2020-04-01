c02fc8610ad275adac21a7088af0b6c2d726c972
YeonjuRyu <ryeonj829@gmail.com>
2020-04-01 18:00:24 +0900

    eslint-typescript, prettier, husky, react-native-storybook

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,35 @@
 module.exports = {
   root: true,
-  extends: '@react-native-community',
+  env: {
+    es2020: true, // es11
+    'jest/globals': true,
+  },
+  extends: [
+    '@react-native-community',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'plugin:react/recommended',
+    'plugin:jest/recommended',
+    'plugin:jest/style',
+    'plugin:prettier/recommended',
+    'prettier/@typescript-eslint',
+  ],
   parser: '@typescript-eslint/parser',
-  plugins: ['@typescript-eslint'],
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: './tsconfig.json',
+    sourceType: 'module', // code run in ECMAScript (import, export... etc.)
+    ecmaFeatures: {
+      jsx: true,
+    },
+  },
+  plugins: ['@typescript-eslint', 'jest', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'error',
+  },
+  reportUnusedDisableDirectives: true,
 };
